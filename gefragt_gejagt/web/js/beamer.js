9b2c6fdcb46b6b0d0d11c9e8acb1199bd4db2c28
JasMich.de <jasper.michalke@jasmich.de>
2019-12-27 10:10:01 +0100

    Added some audio

--- gefragt_gejagt/web/js/beamer.js
+++ gefragt_gejagt/web/js/beamer.js
@@ -102,12 +102,12 @@ $(async function() {
     async function show_playerresponse() {
         console.log('all_show_playerresponse')
         var current_question = (await eel.get_game()()).current_question;
-        if(current_question.answerPlayer==0) {
+        if (current_question.answerPlayer == 0) {
             document.querySelector('#chasePlayerResponse').innerHTML = current_question.correctAnswer;
             document.querySelector('#chasePlayerResponse').classList.remove('wrong');
             document.querySelector('#chasePlayerResponse').classList.add('correct');
         } else {
-            document.querySelector('#chasePlayerResponse').innerHTML = current_question.wrongAnswers[current_question.answerPlayer-1];
+            document.querySelector('#chasePlayerResponse').innerHTML = current_question.wrongAnswers[current_question.answerPlayer - 1];
             document.querySelector('#chasePlayerResponse').classList.remove('correct');
             document.querySelector('#chasePlayerResponse').classList.add('wrong');
         }
@@ -122,12 +122,12 @@ $(async function() {
     async function show_chaserresponse() {
         console.log('all_show_chaserresponse')
         var current_question = (await eel.get_game()()).current_question;
-        if(current_question.answerChaser==0) {
+        if (current_question.answerChaser == 0) {
             document.querySelector('#chaseChserResponse').innerHTML = current_question.correctAnswer;
             document.querySelector('#chaseChserResponse').classList.remove('wrong');
             document.querySelector('#chaseChserResponse').classList.add('correct');
         } else {
-            document.querySelector('#chaseChserResponse').innerHTML = current_question.wrongAnswers[current_question.answerChaser-1];
+            document.querySelector('#chaseChserResponse').innerHTML = current_question.wrongAnswers[current_question.answerChaser - 1];
             document.querySelector('#chaseChserResponse').classList.remove('correct');
             document.querySelector('#chaseChserResponse').classList.add('wrong');
         }
@@ -148,6 +148,10 @@ async function update_beamer() {
 }
 
 async function loadSlideContent(gameStateCode) {
+    document.querySelectorAll('audio').forEach(el => {
+        el.pause();
+        el.load()
+    });
 
     switch (gameStateCode) {
         case gameState.PREPARATION: // PREPARATION
@@ -169,12 +173,19 @@ async function loadSlideContent(gameStateCode) {
         case 4: // FAST_GUESS
             console.log("Schnellraterunde");
             fastGuessTimer = setInterval(async function() {
+                var score;
                 try {
-                    var score = (await eel.get_game()()).current_player.points;
+                    var game = await eel.get_game()();
+                    if (game.state != 4) throw (new Error());
+                    score = game.current_player.points;
                 } catch (e) {
                     clearInterval(fastGuessTimer);
+                    //console.error(e);
+                    return;
                 }
-                document.querySelectorAll('.fast_guess_score').forEach(el => el.innerHTML = score);
+                var oldScore = document.querySelector('#fast_guess_score').innerText;
+                document.querySelector('#fast_guess_score').innerHTML = score;
+                if (score > oldScore) document.querySelector("#success").play();
             }, 250)
             break;
         case 5: // CHASE_PREPARATION
@@ -185,6 +196,7 @@ async function loadSlideContent(gameStateCode) {
                     var offers = (await eel.get_game()()).current_round.offers;
                 } catch (e) {
                     clearInterval(offersIntervall);
+                    return;
                 }
                 document.querySelector('#high_offer').innerHTML = offers.find((el) => el.type == 0).amount;
                 document.querySelector('#normal_offer').innerHTML = offers.find((el) => el.type == 1).amount;
@@ -213,54 +225,61 @@ async function loadSlideContent(gameStateCode) {
             console.log("Jagd Fragenstellung");
             break;
         case 7: // CHASE_SOLVE
-            document.querySelector('#correctAnswer').innerHTML='███'
+            document.querySelector('#correctAnswer').innerHTML = '███'
             document.querySelector('#chasePlayerResponse').classList.remove('show');
             document.querySelector('#chaseChserResponse').classList.remove('show');
-            document.querySelector('#chasePlayerResponse').innerHTML='███'
+            document.querySelector('#chasePlayerResponse').innerHTML = '███'
             document.querySelector('#chasePlayerResponse').classList.remove('correct');
             document.querySelector('#chasePlayerResponse').classList.remove('wrong');
-            document.querySelector('#chaseChserResponse').innerHTML='███'
+            document.querySelector('#chaseChserResponse').innerHTML = '███'
             document.querySelector('#chaseChserResponse').classList.remove('correct');
             document.querySelector('#chaseChserResponse').classList.remove('wrong');
             console.log("Jagd Auflösung");
             break;
         case 8: // ROUND_ENDED
-            var player=(await eel.get_game()()).current_player
-            document.querySelector('#playerWonMessage').innerHTML=player.name + ' hat ' + ((player.qualified) ? 'gewonnen' : 'verloren') + '!';
+            var player = (await eel.get_game()()).current_player
+            document.querySelector((player.qualified) ? '#success' : '#error').play();
+            document.querySelector('#playerWonMessage').innerHTML = player.name + ' hat ' + ((player.qualified) ? 'gewonnen' : 'verloren') + '!';
             console.log("Runde beendet");
             break;
         case 9: // FINAL_PREPARATION
             console.log("Finale Vorbereitung");
             break;
         case 10: // FINAL_PLAYERS
-            document.querySelector('#finalQuestionPlayer').innerHTML=(await eel.get_game()()).current_question.text
+            document.querySelector('#finalQuestionPlayer').innerHTML = (await eel.get_game()()).current_question.text
             console.log("Finale Spieler*in");
             break;
         case 11: // FINAL_BETWEEN
+            document.querySelector("#waiting").play();
             console.log("Finale Zwischenpause");
             break;
         case 12: // FINAL_CHASER
-            document.querySelector('#finalQuestionChaser').innerHTML=(await eel.get_game()()).current_question.text
+            document.querySelector('#finalQuestionChaser').innerHTML = (await eel.get_game()()).current_question.text
             console.log("Status: Finale Jäger*in");
             break;
         case 13: // FINAL_CHASER_WRONG
+            document.querySelector("#error").play();
             console.log("Finale Jäger*in Falschantwort");
             break;
         case 14: // FINAL_END
+            document.querySelector("#waiting").play();
             console.log("Ende");
             break;
         case 15: // EVALUATION
-            var game=await eel.get_game()();
-            var evaluation=game.current_round;
-            document.querySelector('#evaluationMessage').innerHTML=game.current_team.name + ' hat ' + ((evaluation.won) ? 'gewonnen' : 'verloren') + '!';
-            document.querySelector('#evaluationScorePlayer').innerHTML=evaluation.correctAnswersPlayer;
-            document.querySelector('#evaluationScoreChaser').innerHTML=evaluation.correctAnswersChaser;
-            if(evaluation.won) {
+            var game = await eel.get_game()();
+            var evaluation = game.current_round;
+            document.querySelector('#evaluationMessage').innerHTML = game.current_team.name + ' hat ' + ((evaluation.won) ? 'gewonnen' : 'verloren') + '!';
+            document.querySelector('#evaluationScorePlayer').innerHTML = evaluation.correctAnswersPlayer;
+            document.querySelector('#evaluationScoreChaser').innerHTML = evaluation.correctAnswersChaser;
+            if (evaluation.won) {
+                document.querySelector("#success").play();
                 document.querySelector('#evaluationScorePlayer').classList.add('correct');
                 document.querySelector('#evaluationScorePlayer').classList.remove('wrong');
                 document.querySelector('#evaluationScoreChaser').classList.add('wrong');
                 document.querySelector('#evaluationScoreChaser').classList.remove('correct');
+                document.querySelector("#success").play();
             } else {
+                document.querySelector("#error").play();
                 document.querySelector('#evaluationScoreChaser').classList.add('correct');
                 document.querySelector('#evaluationScoreChaser').classList.remove('wrong');
                 document.querySelector('#evaluationScorePlayer').classList.add('wrong');
@@ -269,7 +288,7 @@ async function loadSlideContent(gameStateCode) {
             console.log("Auswertung");
             break;
         default:
-            document.getElementById("status").innerHTML = "Status: Unbekannt " + game.state;
+            console.log("Status: Unbekannt " + game.state);
             break;
     }
 }
